ecf395ef5cc891cca26fd1bfb861e1c731c66442
Felipe <feelipe.ss31@gmail.com>
2025-07-08 12:51:40 -0300

    add simple page system

---
+++ README.md
@@ -0,0 +1,29 @@
+# NLW Agents (Web)
+
+Projeto desenvolvido durante o evento NLW da Rocketseat.
+
+## Tecnologias Utilizadas
+
+- React
+- TypeScript
+- Vite
+
+## Padrões de Projeto
+
+- Componentização com React
+- Organização de páginas e componentes em pastas separadas
+
+## Como rodar o frontend
+
+1. Instale as dependências:
+   ```bash
+   npm install
+   ```
+2. Inicie o projeto:
+   ```bash
+   npm run dev
+   ```
+
+---
+
+Projeto criado no evento NLW da Rocketseat.
---
+++ src/pages/create-room.tsx
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
+
+type GetRoomsAPIResponse = Array<{
+    id: string
+    name: string
+}>
+
+export const CreateRoom = () => {
+    const { data, isLoading } = useQuery({
+        queryKey: ['get-room'],
+        queryFn: async () => {
+            const response = await fetch('http://localhost:3333/rooms')
+            const result: GetRoomsAPIResponse = await response.json()
+
+            return result
+        },
+    })
+
+    return (
+        <div className="flex flex-col gap-0.5">
+            {isLoading && <p>Carregando.....</p>}
+            {data?.map((room) => {
+                return (
+                    <Link key={room.id} to={`/room/${room.id}`}>
+                        {room.name}
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
